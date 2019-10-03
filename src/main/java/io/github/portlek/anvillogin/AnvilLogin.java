d376c7b6ad2152fcdceebcdd67600f79985f95f9
portlek <hsndmrts_@outlook.com>
2019-10-04 00:29:59 +0300

    Update AnvilLogin.java

--- src/main/java/io/github/portlek/anvillogin/AnvilLogin.java
+++ src/main/java/io/github/portlek/anvillogin/AnvilLogin.java
@@ -7,7 +7,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AnvilLogin extends JavaPlugin implements Listener {
